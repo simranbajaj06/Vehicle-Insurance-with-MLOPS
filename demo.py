e78649d700e0dc311886771b5e54cc37b6e523b6
Simran Bajaj <Simran.Bajaj@friska.ai>
2026-09-18 17:14:47 +0530

    testing

--- demo.py
+++ demo.py
@@ -23,4 +23,4 @@ from src.pipline.training_pipeline import TrainPipeline
 pipline = TrainPipeline()
 pipline.run_pipeline()
 
-print("ending the Vehicle Insurance Prediction with MLOps demo script.")
+print("ending the Vehicle Insurance Prediction with MLOps demo script. ")
